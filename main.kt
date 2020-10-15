88ee5b776830e1ad12818f06bd7e96d78eec61d8
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2020-10-15 01:12:59 -0500

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var myByte : Byte = 1
+    var myShort : Short = 4
+    var myInt : Int = 69
+    var myLong : Long = 666L
+    var myFloat : Float = 2.24F
+    var myDouble : Double = 3.14159265359
+    var myBool : Boolean = true
+    var myChar : Char = 'z'
+
+    println("My Byte example is: $myByte")
+    println("My Short example is: $myShort")
+    println("My Int example is: $myInt")
+    println("My Long example is: $myLong")
+    println("My Float example is: $myFloat")
+    println("My Double example is: $myDouble")
+    println("My Boolean example is: $myBool")
+    println("My Char example is: $myChar")
+}
